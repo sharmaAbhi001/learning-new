2bc947509c461a0a2e5f0e9db70a00bc95b7a4f5
sharmaAbhi001 <insanepanther97@gmail.com>
2024-10-25 22:17:45 +0530

    first

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("hello bahi ");
+let count =0;
+document.querySelector(".click").addEventListener("click",()=>{
+    
+document.querySelector(".count").innerHTML = count+1;
+count = count+1;    
+})
